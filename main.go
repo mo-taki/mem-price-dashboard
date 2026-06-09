4aa17c703669a09061629f111a85f980d011d60f
mo-taki <ewok.mfk@gmail.com>
2026-06-10 00:13:11 +0900

    feat: schema.sqlからSQLiteをセットアップ

--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 	"os"
 
+	_ "embed"
+
+	_ "modernc.org/sqlite"
+
 	"github.com/joho/godotenv"
 )
 
+//go:embed schema.sql
+var schemaSQL string
+
 func main(){
-	
 	godotenv.Load()
 	apiKey := os.Getenv("JQUANTS_API_KEY")
 	fmt.Println("API Key:", apiKey)
-	
+
 	api_url := "https://api.jquants.com/v2/equities/bars/daily"
 	params := url.Values{}
 	params.Set("code", "285A0")
@@ -41,6 +48,19 @@ func main(){
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		fmt.Println("Error:", err)
+		return
+	}
+
+	db, err := sql.Open("sqlite", "./stock_prices.db")
+	if err != nil {
+		fmt.Println("Error opening database:", err)
+		return
+	}
+	defer db.Close()
+
+	_, err = db.Exec(schemaSQL)
+	if err != nil {
+		fmt.Println("Error executing schema:", err)
 		return
 	}
 
