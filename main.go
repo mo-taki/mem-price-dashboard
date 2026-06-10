01889aa37546ee111c3272e470d66774a02eadda
mo-taki <ewok.mfk@gmail.com>
2026-06-11 01:01:27 +0900

    feat: APIレスポンスの株価をDBにUPSERT

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -17,6 +18,27 @@ import (
 
 //go:embed schema.sql
 var schemaSQL string
+
+type StockPriceResponse struct {
+	Data []struct {
+		Date      string  `json:"Date"`
+		Code      string  `json:"Code"`
+		O         float64 `json:"O"`
+		H         float64 `json:"H"`
+		L         float64 `json:"L"`
+		C         float64 `json:"C"`
+		Ul        string  `json:"UL"`
+		Ll        string  `json:"LL"`
+		Vo        float64 `json:"Vo"`
+		Va        float64 `json:"Va"`
+		AdjFactor float64 `json:"AdjFactor"`
+		AdjO      float64 `json:"AdjO"`
+		AdjH      float64 `json:"AdjH"`
+		AdjL      float64 `json:"AdjL"`
+		AdjC      float64 `json:"AdjC"`
+		AdjVo     float64 `json:"AdjVo"`
+	} `json:"data"`
+}
 
 func main(){
 	godotenv.Load()
@@ -64,5 +86,23 @@ func main(){
 		return
 	}
 
-	fmt.Println("Response:", string(body))
+	var stockPriceResponse StockPriceResponse
+	err = json.Unmarshal(body, &stockPriceResponse)
+	if err != nil {
+		fmt.Println("Error unmarshalling response:", err)
+		return
+	}
+
+	insertSQL := `INSERT INTO stock_prices (code, date, open, high, low, close, volume, adj_close, adj_factor)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?) ON CONFLICT(code, date)
+		DO UPDATE SET open=excluded.open, high=excluded.high, low=excluded.low, close=excluded.close, volume=excluded.volume, adj_close=excluded.adj_close, adj_factor=excluded.adj_factor`
+	for _, data := range stockPriceResponse.Data {
+		_, err = db.Exec(insertSQL,
+			data.Code, data.Date, data.O, data.H, data.L, data.C, data.Vo, data.AdjC, data.AdjFactor)
+		if err != nil {
+			fmt.Println("Error inserting data:", err)
+			return
+		}
+		fmt.Println("Inserted/Updated:", data.Date, data.AdjC)
+	}
 }
