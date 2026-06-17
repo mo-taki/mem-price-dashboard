55b2e0dee753556f4ab33a1f274d1f12b14d2270
mo-taki <ewok.mfk@gmail.com>
2026-06-18 00:02:14 +0900

    chore: 設定をconstに移動

--- main.go
+++ main.go
@@ -9,11 +9,14 @@ import (
 	"github.com/mo-taki/mem-price-dashboard/internal/store"
 )
 
+const (
+	stockCode = "285A0" // Kioxia
+	dbPath    = "./stock_prices.db"
+)
+
 func main() {
 	godotenv.Load()
 	apiKey := os.Getenv("JQUANTS_API_KEY")
-	stockCode := "285A0" // Kioxia
-	dbPath := "./stock_prices.db"
 
 	client := jquants.New(apiKey)
 	prices, err := client.FetchStockPrices(stockCode)
