71cfb0c28230524bc846333c35dc59e24855f3af
mo-taki <ewok.mfk@gmail.com>
2026-06-21 01:11:52 +0900

    feat: store に読み取りメソッド ListPrices を追加

--- internal/store/store.go
+++ internal/store/store.go
@@ -20,6 +20,18 @@ type Store struct {
 	db *sql.DB
 }
 
+type Price struct {
+	Code      string  `json:"code"`
+	Date      string  `json:"date"`
+	Open      float64 `json:"open"`
+	High      float64 `json:"high"`
+	Low       float64 `json:"low"`
+	Close     float64 `json:"close"`
+	Volume    float64 `json:"volume"`
+	AdjClose  float64 `json:"adjClose"`
+	AdjFactor float64 `json:"adjFactor"`
+}
+
 func Open(path string) (*Store, error) {
 	db, err := sql.Open("sqlite", path)
 	if err != nil {
@@ -46,4 +58,30 @@ func (s *Store) UpsertPrice(p jquants.StockPrice) error {
 
 func (s *Store) Close() error {
 	return s.db.Close()
+}
+
+func (s *Store) ListPrices(code string) ([]Price, error) {
+	q := `SELECT code, date, open, high, low, close, volume, adj_close, adj_factor FROM stock_prices WHERE code = ?;`
+	rows, err := s.db.Query(q, code)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var prices []Price
+
+	for rows.Next() {
+		var p Price
+		if err := rows.Scan(&p.Code, &p.Date, &p.Open, &p.High, &p.Low, &p.Close, &p.Volume, &p.AdjClose, &p.AdjFactor); err != nil {
+			return nil, err
+		}
+		prices = append(prices, p)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return prices, nil
+
 }
